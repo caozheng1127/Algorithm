e135a62498317bb0b4b12fa4439a0c18ff36b64c
caozheng <caozheng1127@gmail.com>
2022-06-05 20:29:39 +0800

    misc:init

--- algorithm-compose/src/main/java/SwordOffer/Trees.java
+++ algorithm-compose/src/main/java/SwordOffer/Trees.java
@@ -263,22 +263,22 @@ public class Trees {
         t.right = Deserialize();
         return t;
     }
-
-    private TreeNode ret;
-    private int cnt = 0;
-
-    public TreeNode KthNode(TreeNode pRoot, int k) {
-        inOrder(pRoot, k);
-        return ret;
-    }
-
-    private void inOrder(TreeNode root, int k) {
-        if (root == null || cnt >= k)
-            return;
-        inOrder(root.left, k);
-        cnt++;
-        if (cnt == k)
-            ret = root;
-        inOrder(root.right, k);
-    }
+//
+//    private TreeNode ret;
+//    private int cnt = 0;
+//
+//    public TreeNode KthNode(TreeNode pRoot, int k) {
+//        inOrder(pRoot, k);
+//        return ret;
+//    }
+//
+//    private void inOrder(TreeNode root, int k) {
+//        if (root == null || cnt >= k)
+//            return;
+//        inOrder(root.left, k);
+//        cnt++;
+//        if (cnt == k)
+//            ret = root;
+//        inOrder(root.right, k);
+//    }
 }
